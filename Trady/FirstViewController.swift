0008e0d3e0e0bb79ef6d0731ae2950fa2f345610
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-10-31 23:16:35 +0200

    Initial Commit

---
+++ Trady/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Trady
+//
+//  Created by Sherif Kamal on 10/31/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Trady/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Trady
+//
+//  Created by Sherif Kamal on 10/31/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
